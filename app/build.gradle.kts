3055a05c56cecf4bc6e43b70ac1c5bb48727994d
Doloreshazze <daw81bk@gmail.com>
2026-09-21 11:17:42 +0200

    Prepare Sphere Launcher 10.0 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.playeverywhere.spherelauncher"
         minSdk = 24
         targetSdk = 36
-        versionCode = 9
-        versionName = "9.0"
+        versionCode = 10
+        versionName = "10.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
